39e76227d274d7b6a5565d224d39c9a9eea7ab2e
Sajuna94 <sajuna94@gmail.com>
2026-04-05 10:41:23 +0800

    aaa

--- lib/hooks/idk/group.ts
+++ lib/hooks/idk/group.ts
@@ -4,10 +4,12 @@ import { useMutation } from "@tanstack/react-query";
 import { useUserQuery } from "../auth/user";
 
 export function useLeaveGroupMutation() {
-	const user = useUserQuery().data;
+	const { data: user } = useUserQuery();
 
 	return useMutation({
 		mutationFn: async (groupId: string) => {
+			if (!user) throw new Error("User not found");
+
 			const { error } = await supabase
 				.from("group_members")
 				.delete()
